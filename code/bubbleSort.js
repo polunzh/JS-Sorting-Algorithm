5891f2462e213eae4be41b034b4952997c38d1a6
polun <965076377@qq.com>
2017-01-22 12:39:36 +0800

    add buble sort implementation code

---
+++ code/bubbleSort.js
@@ -0,0 +1,16 @@
+function bubbleSort(arr) {
+    const length = arr.length;
+    for (let i = 0; i < length - 1; i++) {
+        for (let j = 0; j < length - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+const arr = [12, 3, 10, 11, 1];
+bubbleSort(arr);
+console.log(arr);
